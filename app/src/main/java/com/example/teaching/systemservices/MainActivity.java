554e5cddfaae2087ea130a9ea473762d5aca7921
marek.laskowski <test>
2017-03-21 13:22:17 -0400

    added (untested) intent filter and broadcast receiver in MainActivity

--- app/src/main/java/com/example/teaching/systemservices/MainActivity.java
+++ app/src/main/java/com/example/teaching/systemservices/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.teaching.systemservices;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +14,25 @@ import android.widget.Button;
  * 1) how to start and stop a service.
  */
 public class MainActivity extends AppCompatActivity {
+
+    //member field to store an reference to our MyBroadcastReceiver instance.. instantiate it onCreate()
+    MyBroadcastReceiver myBroadcastReceiver = null;
+
+    /**
+     * BroadcastReciever is a class for receiving broadcasted Intents
+     * We will instantiate and register an BroadcastReciever to listen for Intents
+     * sent by our service.
+     * Start listenting for Intents when the Activity starts
+     * Stop listenting for Intents when the Activity is no longer at the foreground (onPause)
+     */
+    public class MyBroadcastReceiver extends BroadcastReceiver{
+        //onRecieve will be called by the operating system when an Intent that matches the filter is received
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //call a method defined in MainActivity...
+            handleIntent(intent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +56,36 @@ public class MainActivity extends AppCompatActivity {
                 stopMyService();
             }
         });
+
+        //instantiate MyBroadcastReceiver for later use...
+        myBroadcastReceiver = new MyBroadcastReceiver();
+    }
+
+    /**
+     * onResume is called when our activity comes back to the foreground
+     * see android activity lifecycle
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*
+        register our BroadcastReceiver
+        ... in order to recieve broadcast Intents we need to create an IntentFilter
+        and then register the BroadcastReceiver
+         */
+        IntentFilter intentFilter = new IntentFilter("com.example.marek.NOTIFICATION_CLICKED");
+        registerReceiver(myBroadcastReceiver, intentFilter);
+    }
+
+    /**
+     * we need to also de-register the BroadcastReceiver when our activty is no longer in the foreground
+     *
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //unregister the BroadcastReceiver to stop listening for updates
+        unregisterReceiver(myBroadcastReceiver);
     }
 
     /**
@@ -57,5 +109,9 @@ public class MainActivity extends AppCompatActivity {
         //stop the service by calling Context.stopService()
         System.out.println("Attempting to stop MyService");
         stopService(intent);
+    }
+
+    private void handleIntent(Intent intent){
+        System.out.println("Our activity received an intent: " + intent.toString());
     }
 }
